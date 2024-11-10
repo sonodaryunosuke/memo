13e654ec8b475059968454ab547bab49bd1f4082
ryunosuke29 <riunoske27@gmail.com>
2024-11-11 05:34:41 +0900

    aaa

---
+++ Workoutmemo/src/main/java/memo_main/workmemo.java
@@ -0,0 +1,51 @@
+package memo_main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class workmemo
+ */
+@WebServlet("/sinin_servlet")
+public class workmemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		request.setCharacterEncoding("UTF8");
+        String email = request.getParameter("email");    
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        // 取得したパラメータを使用した処理
+        response.setContentType("text/html;charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.println("Username: " + username);
+        out.println("Password: " + password);
+        out.println("Password: " + email);
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("<title>memo sinin完了</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>サインインが完了しました</h2>");
+        out.println("<a href=\"login.html\">login画面へ</a>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+}
