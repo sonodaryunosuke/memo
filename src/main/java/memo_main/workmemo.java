4ad07ae6fa61e45e79864c736a9d48d9c57cc9ca
A <abc@a.com>
2024-11-15 16:29:47 +0900

    aaa

--- src/main/java/memo_main/workmemo.java
+++ src/main/java/memo_main/workmemo.java
@@ -1,42 +1,68 @@
 package memo_main;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  * Servlet implementation class workmemo
  */
-@WebServlet("/workmemo")
+@WebServlet("/sinin_servlet")
 public class workmemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public workmemo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served ").append(request.getContextPath());
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+
+		String url = "jdbc:mysql://localhost:3307/mysql?serverTimezone=Asia/Tokyo";
+
+		String user = "root";
+		String passwd = "ktcpass23";
+		
+		try (Connection con = DriverManager.getConnection(url,user,passwd)){
+		System.out.println("connect");
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		
+		
+		
+		
+		
+		request.setCharacterEncoding("UTF8");
+        String email = request.getParameter("email");    
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        // 取得したパラメータを使用した処理
+        response.setContentType("text/html;charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.println("Username: " + username);
+        out.println("Password: " + password);
+        out.println("Password: " + email);
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("<title>memo sinin完了</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>登録が完了しました</h2>");
+        out.println("<a href=\"login.html\">login画面へ</a>");
+        out.println("</body>");
+        out.println("</html>");
 	}
 
 }
